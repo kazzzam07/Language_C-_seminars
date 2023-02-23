2d9469f6ef25d57001b8c03027f9ecae0973bfd6
Kazzzam07 <Kazzzam07@gmail.com>
2023-02-23 15:13:33 +0300

    add task01 and Task05

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Является ли первое число квадратом второго");
+Console.WriteLine("Введите первое число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine("Является");
+}
+else 
+{
+   Console.WriteLine("Не является"); 
+}
---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0)
+{
+    int count = -number;
+    while(count <= number)
+    {
+        Console.Write(count + " ");
+        count = count + 1;
+    }
+}
+else
+{
+    Console.WriteLine("Введено ненатуральное число");
+}
