ab5553ad1961cb9f0bef752b80916c082d9732d0
Kazzzam07 <Kazzzam07@gmail.com>
2023-02-23 18:09:41 +0300

    add Task02, Task04

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Input number A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Больше число - {numberA}, меньшее число - {numberB}");
+}
+else
+{
+    Console.WriteLine($"Больше число - {numberB}, меньшее число - {numberA}");
+}
+
---
+++ Task07/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0 ) number = -number;
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра числа -> {lastDigit}");
+}
