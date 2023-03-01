c9a8fca8e2bc2a5c70afd7a9b46db368a4012329
Kazzzam07 <Kazzzam07@gmail.com>
2023-03-01 11:06:12 +0300

    add Task14 and Task16

---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 7 == 0)
+// {
+//     if (number % 23 == 0) Console.WriteLine("Кратно");
+// }
+// else Console.WriteLine ("Не кратно");
+
+
+Console.WriteLine (MetodMulti(number)? "Кратно":"Не кратно");
+
+
+bool MetodMulti(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Первое число -> ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число -> ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int square1 = number1 * number1;
+// int square2 = number2 * number2;
+
+// if (number1 == square2) Console.WriteLine("+");
+// if (number2 == square1) Console.WriteLine("+");
+// else Console.WriteLine("-");
+
+Console.WriteLine (SquareMetod(number1, number2)? "+":"-");
+
+bool SquareMetod (int num1, int num2)
+{
+    return num1 == num2*num2 || num2 == num1*num1;
+}
