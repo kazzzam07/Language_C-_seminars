863c4b201b728a9758bceddac39886fed6c0f110
Kazzzam07 <Kazzzam07@gmail.com>
2023-03-01 14:22:22 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = new Random().Next();
+Console.WriteLine($"Число -> {number}");
+
+if (number < 100) Console.WriteLine("третьей цифры нет");
+else
+{
+    while (number >= 999)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = ThirdDigit(number);
+    Console.WriteLine($"Thirt digit -> {thirdDigit}");
+}
+
+int ThirdDigit(int num)
+{
+    return num % 10;
+}
