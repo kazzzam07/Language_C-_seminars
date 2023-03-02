465bc10c4e44eb7c10ebc0bc6d8c7cf5c45a2825
Kazzzam07 <Kazzzam07@gmail.com>
2023-03-02 16:30:50 +0300

    add Task17, 18, 20, 22

---
+++ Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿//Зачадча 18 Программа по заданному номеру четверти показывает диапазон
+
+Console.WriteLine("Введите номер четверти");
+string numQuarter = Console.ReadLine();
+Console.WriteLine(Range(numQuarter));
+
+string Range(string num)
+{
+    if (num == "1") return "x > 0, y > 0";
+    if (num == "2") return "x < 0, y > 0";
+    if (num == "3") return "x < 0, y < 0";
+    if (num == "4") return "x > 0, y < 0";
+    return "Введен некорректный номер";
+}
