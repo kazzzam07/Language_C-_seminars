862aac8ea5e3dbbfba34cb3c3a484592dde7b501
KazzzaM07 <Kazzzam@mail.ru>
2023-03-09 18:27:36 +0300

    add tasks from the seminar 5

---
+++ Task37/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+//         else Console.Write($"{arr[i]}");
+//     }
+//     Console.WriteLine("]");
+// }
+
+// int[] CompositOfNumbers(int[] arr)
+// {
+//     int sizeArr2 = 0;
+//     if (arr.Length % 2 == 0) sizeArr2 = arr.Length / 2;
+//     else sizeArr2 = arr.Length / 2 + 1;
+//     int[] resArr = new int[sizeArr2];
+
+//     int indMin = 0;
+//     int indMax = arr.Length - 1;
+//     while (indMin < indMax)
+//     {
+//         int result = resArr[indMin] * resArr[indMax];
+//         indMin++;
+//         indMax--;
+//     }
+//     return resArr;
+// }
+// int compositOfNumbers = CompositOfNumbers;
+
+
+int[] CreateArrayRndInt(int size, int min, int max)//Задаем массив
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)//Выводим массив на экран
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] CompArray(int[] arr)//Функция вычисления произведений
+{
+    int size = arr.Length;
+    int sizeNewArray = size % 2 == 0 ? size / 2: size / 2 + 1;
+    int[] temp = new int[sizeNewArray];
+    for (int i = 0; i < sizeNewArray; i++)
+    {
+        temp[i] = arr[i] * arr[size - i - 1];  //ЗАполняю массив для вывода
+    }
+    if (size % 2 != 0) temp[sizeNewArray - 1] = arr[size / 2]; //Вводим в конец число, если размер массива нечетный
+    return temp;
+}
+
+int[] array = CreateArrayRndInt(5, 0, 9);
+PrintArray(array);
+int[] result = CompArray(array);
+PrintArray(result);
