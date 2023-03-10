372d85cd65ffd325e7ef69bc791ee42f4b722cb0
Kazzzam07 <Kazzzam07@gmail.com>
2023-03-10 12:11:13 +0300

    add task for homework 4

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите натуральное число A -> ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B -> ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int ExponentiationNum(int numA, int numB)
+{
+    int exp = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        exp = exp * numA;
+    }
+    return exp;
+}
+
+int exponentiationNum = ExponentiationNum(numberA, numberB);
+Console.WriteLine($"Число {numberA} в натуральной степени числа {numberB} -> {exponentiationNum}");
--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите натуральное число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int count = Convert.ToString(num).Length;
+    int advance = 0;
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        advance = num - num % 10;
+        sum = sum + (num - advance);
+        num = num / 10;
+    }
+    return sum;
+}
+
+int sumNum = SumNumbers(number);
+Console.WriteLine($"Cуммa цифр в числе {number} -> {sumNum}");
+
+
+
+
